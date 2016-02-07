fde7cb6464f10652f06f0bf364455ad831f2c84c
Nateowami <nateowami@gmail.com>
2016-02-07 20:26:58 +0800

    Overhaul header to match logo & add kids page

--- router.js
+++ router.js
@@ -2,6 +2,9 @@ var express = require("express");
 var sassMiddleware = require('node-sass-middleware');
 var path = require('path');
 var app = express.Router();
+var db = require("monk")("localhost:27017/hftl");
+var kids = db.get("kidscollection");
+var moment = require("moment");
 
 app.use(require("morgan")("combined"));
 
@@ -19,6 +22,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 //normal routes
 app.get('/', function(req, res){
   res.render('home');
+});
+
+app.get('/kids', function(req, res){
+  kids.find({current: true},
+            {sort: {dob: 1}},
+            function(e, kids){
+    res.render("kids/index", {title: "Kids", kids: kids, moment: moment});
+  });
+});
+
+app.get('/kids/:name', function(req, res, next){
+  kids.findOne({name: req.params.name},
+            function(e, kid){
+    res.render("kids/kid", {title: kid.name, kid: kid, moment: moment});
+  })
 });
 
 //errors
